c0c1b23e414990f8bd6e61b39a23e4968e1f0e5b
vit-92 <vitaliy.malyarenko92@gmail.com>
2019-03-23 18:58:25 +0200

    feat: implement getLoveTrianglesCount method

--- src/index.js
+++ src/index.js
@@ -3,5 +3,20 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+    let count = 0;
+    const preferencesLength = preferences.length;
+
+    for (let i = 0; i < preferencesLength; i++) {
+        const isDoesFirstLoverLoveHimself= (preferences[i] != i + 1);
+        const isDoesSecondLoverLoveHimself = preferences[preferences[i] - 1] != preferences[i];
+        const isDoesThirdLoverLoveHimself = preferences[preferences[preferences[i] - 1] - 1] != preferences[preferences[i] - 1];
+        const isDoesThirdLoverLoveFirstLover = preferences[preferences[preferences[i] - 1] - 1] == i + 1;
+
+        if (isDoesFirstLoverLoveHimself && isDoesSecondLoverLoveHimself
+            && isDoesThirdLoverLoveHimself && isDoesThirdLoverLoveFirstLover) {
+            count++;
+        }
+    }
+
+    return count / 3;
 };
